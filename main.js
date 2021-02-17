12e044e02a2e3a14c991e5acff9bbef70748f44d
Positron832 <72881516+Positron832@users.noreply.github.com>
2021-02-17 22:01:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 'strict mode';
-let myHeading = "document.querySelector('h1');
+let myHeading = document.querySelector('h1');
 
 let myName = prompt('Enter your username');
 localStorage.setItem('name', myName);
